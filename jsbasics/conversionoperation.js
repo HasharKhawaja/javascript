b41f1995efbf6190dc39cbef393d6ed90146d8d7
HasharKhawaja <khawaja.hashar.kh@gmail.com>
2024-05-04 16:28:38 +0000

    container config done

---
+++ jsbasics/conversionoperation.js
@@ -0,0 +1,13 @@
+let score=33
+let runs="33"
+console.log(typeof score)
+console.log(typeof runs)
+
+let isloggedIn="1"
+let isthisLoggedIn="hashar"
+let convertToNumber=Number(runs)
+let boleanisLogedIn=Boolean(isloggedIn)
+let IsboleanLogedIn=Boolean(isthisLoggedIn)
+console.log(boleanisLogedIn)
+console.log(IsboleanLogedIn)
+console.log(typeof convertToNumber)
