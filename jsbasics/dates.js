16489e1726ba30634a0d8e81acea4a6cc96e49b7
HasharKhawaja <khawaja.hashar.kh@gmail.com>
2024-05-05 14:24:14 +0000

    container config done

---
+++ jsbasics/dates.js
@@ -0,0 +1,11 @@
+let myDate=new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toISOString())
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleString())
+const mycreatedDate=new Date(2023,0,2,5,30) //month starts from zero in js
+console.log(mycreatedDate.toLocaleString());
