f4e736336e78b7668d6573ee98117a908cfec950
HasharKhawaja <khawaja.hashar.kh@gmail.com>
2024-05-05 13:06:42 +0000

    container config done

---
+++ jsbasics/numberandmaths.js
@@ -0,0 +1,16 @@
+//*************************numbers***************************************** */
+const score=400
+const balance=new Number(100)
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+const otherNumber=12.9876
+// console.log(otherNumber.toPrecision(4));
+const hundreds=1000000
+// console.log(hundreds.toLocaleString());
+//*******************************Maths********************************************** */
+console.log(Math.abs(-4))
+console.log(Math.round(6.9))
+console.log(Math.ceil(6.9))
+console.log(Math.floor(6.9))
