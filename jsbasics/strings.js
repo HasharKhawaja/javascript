0f35cdbe55e3a9784cde4821dbc87c03a5b63064
HasharKhawaja <khawaja.hashar.kh@gmail.com>
2024-05-05 12:28:55 +0000

    container config done

--- jsbasics/strings.js
+++ jsbasics/strings.js
@@ -1,11 +1,19 @@
-const name= "Hashar"
+const name= "Hashar-khawaja-ahmed"
 const age=23
 console.log(`My name is ${name} and my age is ${age}`)
 const GameName= new String("Cricket")
-console.log(GameName)
-console.log(GameName.length)
-console.log(GameName.toUpperCase())
-console.log(GameName.charAt(3))
-console.log(GameName.indexOf("t"))
+// console.log(GameName)
+// console.log(GameName.length)
+// console.log(GameName.toUpperCase())
+// console.log(GameName.charAt(3))
+// console.log(GameName.indexOf("C"))
 const NewString=GameName.substring(0,4)
-console.log(NewString)
+// console.log(NewString)
+const anotherString=GameName.slice(-6,1)
+// console.log(anotherString)
+const newstringOne="   hashar  "
+console.log(newstringOne)
+console.log(newstringOne.trim()) //remove spaces
+const url="https://hashar.com/hashar%20khawaja"
+console.log(url.replace('%20','-'))
+console.log(name.split('-'))
