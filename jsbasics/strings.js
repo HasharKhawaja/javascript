0240d31dc665a0873447162161404b454aca6c6d
HasharKhawaja <khawaja.hashar.kh@gmail.com>
2024-05-05 10:54:34 +0000

    container config done

---
+++ jsbasics/strings.js
@@ -0,0 +1,11 @@
+const name= "Hashar"
+const age=23
+console.log(`My name is ${name} and my age is ${age}`)
+const GameName= new String("Cricket")
+console.log(GameName)
+console.log(GameName.length)
+console.log(GameName.toUpperCase())
+console.log(GameName.charAt(3))
+console.log(GameName.indexOf("t"))
+const NewString=GameName.substring(0,4)
+console.log(NewString)
